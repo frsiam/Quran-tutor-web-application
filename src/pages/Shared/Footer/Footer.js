401117249712394596a13d289c6e71a576ecab19
frsiam <siam.frs@gmail.com>
2022-04-17 13:03:37 +0600

    footer section added

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 const Footer = () => {
+    const year = new Date().getFullYear()
     return (
         <section className="fixed-bottom">
-            footer
+            <footer className="text-center text-white" style={{backgroundColor:'#0a4275'}}>
+                <div className="container p-4 pb-0">
+                    <section className="">
+                        <p className="d-flex justify-content-center align-items-center">
+                            <span className="me-3">Register for free</span>
+                            <button type="button" className="btn btn-outline-light btn-rounded">
+                                Sign up!
+                            </button>
+                        </p>
+                    </section>
+                </div>
+                <div className="text-center p-3" style={{backgroundColor:'rgba(0, 0, 0, 0.2)'}}>
+                    © {year} Copyright :
+                    <span className="text-white"> Quran Tutor</span>
+                </div>
+            </footer>
         </section>
     );
 };
