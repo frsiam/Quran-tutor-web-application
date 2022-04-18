8ec656978556f4f5bd546c8830d1ee04834d9334
frsiam <siam.frs@gmail.com>
2022-04-18 22:48:09 +0600

    notfound page added a imagea and make responsive including a button which redirect to home

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import notFound from '../../images/others/notfound.png';
 const NotFound = () => {
+    const navigate = useNavigate();
+    const handleToHome = () => {
+        navigate('/home')
+    }
     return (
         <div className='container position-relative min-vh-100'>
-            <h1 className='text-danger text-center mt-5 bg-light p-5 shadow rounded'>404, Page not found</h1>
+            <div className='row'>
+                <h1 className='col-12 col-md-6 text-danger text-center mt-5 p-5'>404 Page not found!</h1>
+                <div className='col-12 col-md-6'>
+                    <img className='img-fluid w-100' src={notFound} alt="" />
+                </div>
+            </div>
+            <div className='text-center'>
+                <button onClick={handleToHome} className='btn btn-outline-dark w-50'>Back to Home</button>
+            </div>
         </div>
     );
 };
