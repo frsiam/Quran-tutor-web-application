3955b628aa8574efee11200850f6095fc1df2dc2
frsiam <siam.frs@gmail.com>
2022-04-18 20:16:19 +0600

    about me page done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import banner from '../../images/others/bannerImg.png';
 
 const About = () => {
     return (
-        <div className='position-relative min-vh-100'>
-            <h1 className='text-center bg-primary py-5'>About me</h1>
+        <div className='container my-5 position-relative min-vh-100'>
+            <div className='hero-area row'>
+                <div className="col-12 col-md-6 d-flex align-items-center">
+                    <div>
+                        <h1 className='fw-bold'>Md Foyzur Rahman Siam</h1>
+                        <h3 className='text-secondary'>React js Developer</h3>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6">
+                    <img src={banner} className='img-fluid' alt="" />
+                </div>
+            </div>
+            <div className='mt-5'>
+                <p className='fs-5 text-center'>A Electrical and Electronic Engineering graduate willing to learn new technologies and skills in a short period of time. I want to demonstrate and build my expertise as programmer by seeking the knowledge and skill sets in the areas of new technology, effective and efficient communication and best practice processes. To prove myself as a quick learner and highly energetic person I'm ready to face any challenge and also eager to build myself as a successful teammate.</p>
+            </div>
         </div>
     );
 };
--- src/pages/CheckOut/CheckOut.js
+++ src/pages/CheckOut/CheckOut.js
@@ -6,17 +6,26 @@ import useServices from '../hooks/useServices';
 import Loading from '../Shared/Loading/Loading';
 
 const CheckOut = () => {
-    const [user,loading] = useAuthState(auth)
+    const [user, loading] = useAuthState(auth)
     const { serviceId } = useParams()
     const allData = useServices()
     const singleData = allData?.find(x => x.id === parseInt(serviceId))
     // if(!user){
     //     return <Loading/>
     // }
+    const {name, img, benifits} = singleData;
     return (
-        <div className='position-relative min-vh-100'>
-            <h3>Here checkout our services of id is {serviceId}</h3>
-            <h4>Name of Course: {singleData.name}</h4>
+        <div className='container my-5 position-relative min-vh-100'>
+            <div className="card">
+                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                    <img width={'100%'} style={{height:'400px'}} src={img[1]} className="img-fluid" />
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#!" className="btn btn-primary">Button</a>
+                </div>
+            </div>
         </div>
     );
 };
